2b25b37a6febf402742e84c82a9ff92c33f1c1d2
Kurtis Moffett <moffkr91@gmail.com>
2018-03-06 09:39:48 -0800

    adding buttons

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -1,7 +1,10 @@
 $( document ).ready(function () {
 
 
-let search_term = ""
+
+
+
+let search_term = "cat"
 let queryURL = "https://api.giphy.com/v1/gifs/search?api_key=qb8lbk6ofj2a1OHH6gXZDnJbL6M4s5Zl&q=" + search_term + "&limit=1&offset=0&rating=G&lang=en";
 
 $.ajax({
@@ -11,12 +14,26 @@ $.ajax({
     console.log(response);
 });
 
+let all_buttons = []
 
+function create_buttonz() {
+let a = $("<button>")
+for (i=0; i<all_buttons.length; i++) {
+    a.addClass("buttonz btn btn-primary")
+    a.attr("data-name", all_buttons[i])
+    a.text(all_buttons[i])
+    $("#buttons-span").append(a)
+    }
+}
 
 
 
 $("#click-submit").on("click", function(){
     event.preventDefault();
-    console.log($("#form").text())
+    let button = $("#form-input").val().trim()
+    all_buttons.push(button)
+    create_buttonz()
 })
+
+
 });
