fdd4c0efc1ac47b4e6c187c9fc19b645b93d5c52
Kurtis Moffett <moffkr91@gmail.com>
2018-03-06 11:16:46 -0800

    working

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -1,20 +1,32 @@
 $( document ).ready(function () {
 
 
+let all_buttons = []
 
 
-
-let search_term = "cat"
-let queryURL = "https://api.giphy.com/v1/gifs/search?api_key=qb8lbk6ofj2a1OHH6gXZDnJbL6M4s5Zl&q=" + search_term + "&limit=1&offset=0&rating=G&lang=en";
-
+function generate_gif(){
+    let search_term = $(this).attr("data-name")
+    let queryURL = "https://api.giphy.com/v1/gifs/search?api_key=qb8lbk6ofj2a1OHH6gXZDnJbL6M4s5Zl&q=" + search_term + "&limit=5&offset=0&rating=G&lang=en";
 $.ajax({
     url: queryURL,
     method: 'GET'
 }).then(function(response) {
     console.log(response);
-});
+    console.log(search_term)
+    let gif_div = $("<div class=`gif_div`>")
+    let gif_url = response.data[i].images.original.url
+    let gif = $("<img>").attr("src", gif_url)
+    gif_div.append(gif)
+    $("#gif-span").append(gif_div)
 
-let all_buttons = []
+});//ajax then
+
+}//generate gif
+
+
+
+
+
 
 function create_buttonz() {
 let a = $("<button>")
@@ -25,15 +37,17 @@ for (i=0; i<all_buttons.length; i++) {
     $("#buttons-span").append(a)
     }
 }
-
-
-
+$(document).on("click",".buttonz", generate_gif)
 $("#click-submit").on("click", function(){
     event.preventDefault();
     let button = $("#form-input").val().trim()
     all_buttons.push(button)
     create_buttonz()
 })
+$("#clear-buttons").on("click", function(){
+    event.preventDefault();
+    $("#buttons-span").empty()
+})
 
-
+/////////////////////////////////////////////////////////////
 });
